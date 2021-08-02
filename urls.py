4678cfcd22ec97c7c9b7df10b56305fd58ab31e3
sagaunleashed <83581367+sagaunleashed@users.noreply.github.com>
2021-08-02 15:29:54 +0530

    Initial commit

---
+++ urls.py
@@ -0,0 +1,52 @@
+"""eduvy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from os import name
+from django.conf import settings
+from django.conf.urls import handler404
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ed.urls')),
+    
+    
+       
+    #REST FRAMEWORK URLS
+    path('api/',include('api.urls')),
+    #rest auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # scheme
+    path('api/docs/',include_docs_urls(title="EduvyApi")),
+    path('api/schema/',get_schema_view(title="eduvyApi",version="1.0.0"),name="openapi-schema"),
+
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+handler404 = "ed.views.handle_not_found"
